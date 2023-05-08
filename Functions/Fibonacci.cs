0359e05f01a927c68819eef0a88c59879c56c808
MatiasLealTapia <matiasa.lealt.23@gmail.com>
2023-05-08 15:14:38 -0400

    202305081515 - Actividad 2: Serie Fibonacci, realizada y funcionando.

---
+++ Functions/Fibonacci.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestIndexar.NET.Functions
+{
+    internal class Fibonacci
+    {
+        public static List<int> ListSerie(int serieLength)
+        {
+            int x = 0;
+            int y = 1;
+            int z = 0;
+            List<int> serie = new List<int> { 0, 1 };
+
+            for (int i = 0; i < serieLength; i++)
+            {
+                z = x + y;
+                serie.Add(z);
+                x = y;
+                y = z;
+            }
+            return serie;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -15,18 +15,18 @@ namespace TestIndexar.NET
     {
         static void Main(string[] args)
         {
-            // Definir variables
+            // Definir variables.
             List<string> actividades = new List<string>
                 {
                     "1. Palindromo",
-                    "2. Fibonnacci",
+                    "2. Serie Fibonacci",
                     "3. Calculadora de notación polaca inversa",
                     "4. Calcular ecuación cuadratica",
                     "5. Calcular IMC"
                 };
-            bool flag = true; // Si "flag" es false, no se volverá a repetir el programa 
+            bool flag = true; // Si "flag" se vuelve false dentro de while, no se volverá a repetir el programa.
 
-            // Empieza programa
+            // Empieza programa.
             Console.WriteLine("Bienvenido al test de conocimientos y habilidades .NET de Indexar.");
 
             while (flag)
@@ -36,20 +36,20 @@ namespace TestIndexar.NET
                 {
                     Console.WriteLine(actividad);
                 }
-                Console.WriteLine("Seleccione alguna actividad ingresando el número (Para salir ingrese 0):");
-                try
+                Console.WriteLine("Seleccione alguna actividad ingresando el número (Para salir ingrese 0):\n");
+                try // try catch por si se introduce un dato que no es integer al input siguiente.
                 {
-                    int numActividad = Convert.ToInt32(Console.ReadLine());
+                    int numActividad = Convert.ToInt32(Console.ReadLine()); // Input para seleccionar el número de actividad.
                     switch (numActividad)
                     {
-
+                        // 1. Palindromos
                         case 1:
                             Console.WriteLine("\nACTIVIDAD 1: PALINDROMOS\n");
-                            Console.WriteLine("Escriba una palabra para determinar si esta es un palindromo.");
+                            Console.WriteLine("Escriba una palabra para determinar si esta es un palindromo:\n");
                             try
                             {
                                 string palindromo = Console.ReadLine();
-                                if (Palindromo.IsPalindromo(palindromo))
+                                if (Palindromo.IsPalindromo(palindromo)) // Llamada a la clase Palindromo dentro de la carpeta Functions.
                                 {
                                     Console.WriteLine("\nLa palabra escrita SÍ es un palindromo.");
                                     Console.WriteLine("\nPalabra normal:");
@@ -59,10 +59,10 @@ namespace TestIndexar.NET
                                 }
                                 else
                                 {
-                                    Console.WriteLine("La palabra escrita NO es un palindromo.");
-                                    Console.WriteLine("Palabra normal:");
+                                    Console.WriteLine("\nLa palabra escrita NO es un palindromo.");
+                                    Console.WriteLine("\nPalabra normal:");
                                     Console.WriteLine(palindromo);
-                                    Console.WriteLine("Palabra en reversa:");
+                                    Console.WriteLine("\nPalabra en reversa:");
                                     Console.WriteLine(Palindromo.ReverseString(palindromo));
                                 }
                             }
@@ -71,6 +71,47 @@ namespace TestIndexar.NET
                                 Console.WriteLine("\n¡ERROR! Debe ser una palabra válida.");
                             }
                             break;
+
+                        // 2. Serie Fibonacci
+                        case 2:
+                            Console.WriteLine("\nACTIVIDAD 2: SERIE FIBONACCI\n");
+                            Console.WriteLine("Escriba un número para definir la longitud de la serie:\n");
+                            try // try catch por si se introduce un dato que no es integer al input siguiente.
+                            {
+                                int longitudSerie = Convert.ToInt32(Console.ReadLine()); // Input para seleccionar la longitud de la serie.
+                                List<int> listaSerieFibonacci = Fibonacci.ListSerie(longitudSerie); // Llamada a la clase Fibonacci dentro de la carpeta Functions.
+                                Console.WriteLine("\nEsta es la serie fibonacci con longitud " + longitudSerie + ":\n");
+                                foreach (int numeroSerie in listaSerieFibonacci)
+                                {
+                                    if (numeroSerie == listaSerieFibonacci.Last())
+                                    {
+                                        Console.Write(numeroSerie + "\n");
+                                    }
+                                    else
+                                    {
+                                        Console.Write(numeroSerie + ",");
+                                    }
+                                }
+                            }
+                            catch (Exception)
+                            {
+                                Console.WriteLine("\n¡ERROR! Debe ser un número válido.");
+                            }
+                            break;
+
+                        // 3. Calculadora de notación polaca inversa
+                        case 3:
+                            break;
+
+                        // 4. Calcular ecuación cuadratica
+                        case 4:
+                            break;
+
+                        // 5. Calcular IMC
+                        case 5:
+                            break;
+
+                            // Si el numero seleccionado no está dentro de la lista entra acá.
                         default:
                             Console.WriteLine("\nPor favor seleccione un número dentro de la lista");
                             break;
@@ -85,7 +126,7 @@ namespace TestIndexar.NET
                 }
                 if (!flag)
                 {
-                    Console.WriteLine("Gracias por usar el programa");
+                    Console.WriteLine("\nGracias por usar el programa\n");
                     break;
                 }
             }
